39990255bbfcf638dda9763a2b959f1fc84f36bd
erfanvaredi <erfanvaredi@outlook.com>
2024-03-06 15:54:03 +0400

    feat: add setup config

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='datamug',
-    version='0.0.1',
-    install_requires=open('requirements.txt').read().splitlines(),
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-)
